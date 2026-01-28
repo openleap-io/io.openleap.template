107bcb65659f7c49785ceeb4ed8de862d388d1b6
Kiril Arsov <kiril.arsov@openleap.io>
2026-01-28 22:59:10 +0100

    updated to use starters

--- src/main/java/io/openleap/template/TemplateServiceApplication.java
+++ src/main/java/io/openleap/template/TemplateServiceApplication.java
@@ -2,8 +2,23 @@ package io.openleap.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "io.openleap.template",
+        "io.openleap.starter.core"
+})
+@EntityScan(basePackages = {
+        "io.openleap.iam.principal",
+        "io.openleap.starter.core"
+})
+@EnableJpaRepositories(basePackages = {
+        "io.openleap.template",
+        "io.openleap.starter.core"
+})
 public class TemplateServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TemplateServiceApplication.class, args);
